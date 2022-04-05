c00fcd6abe10f112c64ca229108ad08041b0c62f
danny6417 <102888250+danny6417@users.noreply.github.com>
2022-04-04 23:47:31 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -542,29 +542,29 @@
 
 // Below this temperature the heater will be switched off
 // because it probably indicates a broken thermistor wire.
-#define HEATER_0_MINTEMP   5
-#define HEATER_1_MINTEMP   5
-#define HEATER_2_MINTEMP   5
-#define HEATER_3_MINTEMP   5
-#define HEATER_4_MINTEMP   5
-#define HEATER_5_MINTEMP   5
-#define HEATER_6_MINTEMP   5
-#define HEATER_7_MINTEMP   5
+#define HEATER_0_MINTEMP   21
+#define HEATER_1_MINTEMP   21
+#define HEATER_2_MINTEMP   21
+#define HEATER_3_MINTEMP   21
+#define HEATER_4_MINTEMP   21
+#define HEATER_5_MINTEMP   21
+#define HEATER_6_MINTEMP   21
+#define HEATER_7_MINTEMP   21
 #define BED_MINTEMP        5
 #define CHAMBER_MINTEMP    5
 
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 275
-#define HEATER_1_MAXTEMP 275
-#define HEATER_2_MAXTEMP 275
-#define HEATER_3_MAXTEMP 275
-#define HEATER_4_MAXTEMP 275
-#define HEATER_5_MAXTEMP 275
-#define HEATER_6_MAXTEMP 275
-#define HEATER_7_MAXTEMP 275
-#define BED_MAXTEMP      150
+#define HEATER_0_MAXTEMP 500
+#define HEATER_1_MAXTEMP 500
+#define HEATER_2_MAXTEMP 500
+#define HEATER_3_MAXTEMP 500
+#define HEATER_4_MAXTEMP 500
+#define HEATER_5_MAXTEMP 500
+#define HEATER_6_MAXTEMP 500
+#define HEATER_7_MAXTEMP 500
+#define BED_MAXTEMP      200
 #define CHAMBER_MAXTEMP  60
 
 /**
